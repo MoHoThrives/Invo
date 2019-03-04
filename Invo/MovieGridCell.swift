526fe87c92104a8bfb4696f014f28f7815c51132
Mohanad <mohanad.osman08@qmail.cuny.edu>
2019-03-03 21:52:53 -0500

    Added the grid view, details view, and also did a little clean up

---
+++ Invo/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Invo
+//
+//  Created by Mohanad Osman on 3/3/19.
+//  Copyright © 2019 mohanadhmd. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
